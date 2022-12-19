643bdcb78b5077b22426cca4047cc624176bf0db
touraghe <touraghen@gmail.com>
2022-12-19 11:28:01 +0100

    finish front-end Navigation.js

---
+++ client/src/components/Navigation.js
@@ -0,0 +1,45 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function Navigation() {
+    return (
+        <>
+            {['xxl'].map((expand) => (
+                <Navbar key={expand} bg="white" expand={expand} className="mb-3">
+                    <Container fluid>
+                        <Navbar.Brand href="/"><b>WE ARE HIRING</b></Navbar.Brand>
+                        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+                        <Navbar.Offcanvas
+                            id={`offcanvasNavbar-expand-${expand}`}
+                            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                            placement="end"
+                        >
+                            <Offcanvas.Header closeButton>
+                                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                                    Offcanvas
+                                </Offcanvas.Title>
+                            </Offcanvas.Header>
+                            <Offcanvas.Body>
+                                <Nav className="justify-content-end flex-grow-1 pe-3">
+                                    <Nav.Link href="/">Home</Nav.Link>
+                                    <Nav.Link href="/offers">Offers</Nav.Link>
+                                    <Nav.Link href="/contact-us">Contact</Nav.Link>
+                                </Nav>
+                            </Offcanvas.Body>
+                        </Navbar.Offcanvas>
+                    </Container>
+                </Navbar>
+            ))}
+
+
+        </>
+
+    )
+}
+
+export default Navigation
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import Navigation from '../components/Navigation'
 
 function Home() {
     return (
-        <div><h1>Home</h1></div>
+        <div><Navigation /></div>
     )
 }
 
